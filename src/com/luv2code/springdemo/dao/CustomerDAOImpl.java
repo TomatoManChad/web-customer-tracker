59c0cb3e8d2bc3060ee80c293a0e87f81fab6c63
TomatoManChad <spankthemonkey15@hotmail.co.uk>
2018-07-22 21:50:44 +0100

    DAO into controller and display customers table into jsp

--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -2,21 +2,36 @@ package com.luv2code.springdemo.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import com.luv2code.springdemo.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 
-	
-	//need to inject the session factory
+	// need to inject the session factory
 	@Autowired
-	
-	
-	
-	
+	public SessionFactory sessionFactory;
+
 	@Override
+	@Transactional
 	public List<Customer> getCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+	//1.get current hibernate session
+	//2. create quiery.
+	//3. exe query and get result list
+	//4. return results
+		
+	Session currentSession = sessionFactory.getCurrentSession();
+	
+	Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+
+	List<Customer> customers = theQuery.getResultList();	
+		
+		return customers;
 	}
 
 }
